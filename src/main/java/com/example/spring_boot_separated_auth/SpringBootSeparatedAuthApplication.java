45d1f22a303d12c6585c8337d409a54d3d0c6b6c
Vuwar <vuqarkrm1@gmail.com>
2025-04-17 18:37:57 +0400

    Base version

---
+++ src/main/java/com/example/spring_boot_separated_auth/SpringBootSeparatedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_separated_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeparatedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeparatedAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_separated_auth/SpringBootSeparatedAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_separated_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSeparatedAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
